cd96bf1ac0aa52de5c62c747a03bb0e7fe8f7003
483446 <483446@f9ac3b5a-4d44-314b-830a-d01ff34c7e1a>
2016-02-16 12:28:37 +0000

    L1T6 Converted TriForce Symbol to use element array buffers.

--- 483446/Labs/Lab1/Lab1Window.cs
+++ 483446/Labs/Lab1/Lab1Window.cs
@@ -8,7 +8,7 @@ namespace Labs.Lab1
 {
     public class Lab1Window : GameWindow
     {
-        private int mVertexBufferObjectID;
+        private int[] mVertexBufferObjectIDArray = new int[2];
         private ShaderUtility mShader;
 
         public Lab1Window()
@@ -30,24 +30,34 @@ namespace Labs.Lab1
         {
             GL.ClearColor(Color4.ForestGreen);
 
-            float[] vertices = new float[] { -0.8f, 0.0f,
-                                             0.0f, 0.0f,
-                                             -0.4f, 0.6f,                                             -0.8f, -0.6f,
-                                             0.0f, -0.6f,
-                                             -0.4f, 0.0f,                                             0.0f, -0.6f,
-                                             0.8f, -0.6f,
-                                             0.4f, 0.0f};
+            float[] vertices = new float[] {-0.4f, 0.0f,
+                                            0.4f, 0.0f,
+                                            0.0f, 0.6f,                                            -0.8f, -0.6f,
+                                            0.0f, -0.6f,
+                                            0.8f, -0.6f};
 
-            GL.GenBuffers(1, out mVertexBufferObjectID);
-            GL.BindBuffer(BufferTarget.ArrayBuffer, mVertexBufferObjectID);
+            int[] indices = new int[] {0, 1, 2, 3, 4, 0, 4, 5, 1};
+
+            GL.GenBuffers(2, mVertexBufferObjectIDArray);
+
+            GL.BindBuffer(BufferTarget.ArrayBuffer, mVertexBufferObjectIDArray[0]);
             GL.BufferData(BufferTarget.ArrayBuffer, (IntPtr)(vertices.Length * sizeof(float)), vertices, BufferUsageHint.StaticDraw);
-
+            
             int size;
             GL.GetBufferParameter(BufferTarget.ArrayBuffer, BufferParameterName.BufferSize, out size);
-
+            
             if (vertices.Length * sizeof(float) != size)
             {
                 throw new ApplicationException("Vertex data not loaded onto graphics card correctly");
+            }
+
+            GL.BindBuffer(BufferTarget.ElementArrayBuffer, mVertexBufferObjectIDArray[1]);
+            GL.BufferData(BufferTarget.ElementArrayBuffer, (IntPtr)(indices.Length * sizeof(uint)), indices, BufferUsageHint.StaticDraw);
+            GL.GetBufferParameter(BufferTarget.ElementArrayBuffer, BufferParameterName.BufferSize, out size);
+            
+            if (indices.Length * sizeof(uint) != size)
+            {
+                throw new ApplicationException("Index data not loaded onto graphics card correctly");
             }
 
             #region Shader Loading Code - Can be ignored for now
@@ -65,7 +75,8 @@ namespace Labs.Lab1
         {
             base.OnRenderFrame(e);
             GL.Clear(ClearBufferMask.ColorBufferBit);
-            GL.BindBuffer(BufferTarget.ArrayBuffer, mVertexBufferObjectID);
+            GL.BindBuffer(BufferTarget.ArrayBuffer, mVertexBufferObjectIDArray[0]);
+            GL.BindBuffer(BufferTarget.ElementArrayBuffer, mVertexBufferObjectIDArray[1]);
 
             // shader linking goes here
             #region Shader linking code - can be ignored for now
@@ -77,7 +88,7 @@ namespace Labs.Lab1
 
             #endregion
 
-            GL.DrawArrays(PrimitiveType.Triangles, 0, 9);
+            GL.DrawElements(PrimitiveType.Triangles, 9, DrawElementsType.UnsignedInt, 0);
 
             this.SwapBuffers();
         }
@@ -85,7 +96,7 @@ namespace Labs.Lab1
         protected override void OnUnload(EventArgs e)
         {
             base.OnUnload(e);
-            GL.DeleteBuffers(1, ref mVertexBufferObjectID);
+            GL.DeleteBuffers(2, mVertexBufferObjectIDArray);
             GL.UseProgram(0);
             mShader.Delete();
         }
