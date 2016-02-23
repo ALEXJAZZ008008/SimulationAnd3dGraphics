8906576d0981768a5c2e3ce890af236cc71167ec
483446 <483446@f9ac3b5a-4d44-314b-830a-d01ff34c7e1a>
2016-02-23 17:20:06 +0000

    L22T8 Made a cube of cubes

--- 483446/Labs/Lab2/Lab2_2Window.cs
+++ 483446/Labs/Lab2/Lab2_2Window.cs
@@ -70,6 +70,42 @@ namespace Labs.Lab2
                 mView = mView * Matrix4.CreateTranslation(0, 0, -cameraSpeed * 10);
                 MoveCamera();
             }
+
+            if (e.KeyChar == 'z')
+            {
+                mView = mView * Matrix4.CreateRotationZ(cameraSpeed);
+                MoveCamera();
+            }
+
+            if (e.KeyChar == 'c')
+            {
+                mView = mView * Matrix4.CreateRotationZ(-cameraSpeed);
+                MoveCamera();
+            }
+
+            if (e.KeyChar == 't')
+            {
+                mView = mView * Matrix4.CreateTranslation(0, -cameraSpeed, 0);
+                MoveCamera();
+            }
+
+            if (e.KeyChar == 'f')
+            {
+                mView = mView * Matrix4.CreateTranslation(cameraSpeed, 0, 0);
+                MoveCamera();
+            }
+
+            if (e.KeyChar == 'g')
+            {
+                mView = mView * Matrix4.CreateTranslation(0, cameraSpeed, 0);
+                MoveCamera();
+            }
+
+            if (e.KeyChar == 'h')
+            {
+                mView = mView * Matrix4.CreateTranslation(-cameraSpeed, 0, 0);
+                MoveCamera();
+            }
         }
 
         private void MoveCamera()
@@ -154,17 +190,192 @@ namespace Labs.Lab2
 
             int uModelLocation = GL.GetUniformLocation(mShader.ShaderProgramID, "uModel");
 
-            Matrix4 m1 = Matrix4.CreateTranslation(0.5f, 0, 0);
-            Matrix4 m2 = Matrix4.CreateRotationZ(0.8f);
+            Matrix4 m1 = Matrix4.CreateRotationZ(0.8f);
+            Matrix4 m2 = Matrix4.CreateTranslation(0, 0, 0);
             Matrix4 m3 = m1 * m2;
             GL.UniformMatrix4(uModelLocation, true, ref m3);
 
             GL.DrawElements(BeginMode.Triangles, mModel.Indices.Length, DrawElementsType.UnsignedInt, 0);
 
             Matrix4 m4 = Matrix4.CreateRotationZ(0.8f);
-            Matrix4 m5 = Matrix4.CreateTranslation(-0.5f, 0, 0);
+            Matrix4 m5 = Matrix4.CreateTranslation(0.3f, 0.3f, 0);
             Matrix4 m6 = m4 * m5;
             GL.UniformMatrix4(uModelLocation, true, ref m6);
+
+            GL.DrawElements(BeginMode.Triangles, mModel.Indices.Length, DrawElementsType.UnsignedInt, 0);
+
+            Matrix4 m7 = Matrix4.CreateRotationZ(0.8f);
+            Matrix4 m8 = Matrix4.CreateTranslation(-0.3f, -0.3f, 0);
+            Matrix4 m9 = m7 * m8;
+            GL.UniformMatrix4(uModelLocation, true, ref m9);
+
+            GL.DrawElements(BeginMode.Triangles, mModel.Indices.Length, DrawElementsType.UnsignedInt, 0);
+
+            Matrix4 m10 = Matrix4.CreateRotationZ(0.8f);
+            Matrix4 m11 = Matrix4.CreateTranslation(0.3f, -0.3f, 0);
+            Matrix4 m12 = m10 * m11;
+            GL.UniformMatrix4(uModelLocation, true, ref m12);
+
+            GL.DrawElements(BeginMode.Triangles, mModel.Indices.Length, DrawElementsType.UnsignedInt, 0);
+
+            Matrix4 m13 = Matrix4.CreateRotationZ(0.8f);
+            Matrix4 m14 = Matrix4.CreateTranslation(-0.3f, 0.3f, 0);
+            Matrix4 m15 = m13 * m14;
+            GL.UniformMatrix4(uModelLocation, true, ref m15);
+
+            GL.DrawElements(BeginMode.Triangles, mModel.Indices.Length, DrawElementsType.UnsignedInt, 0);
+
+            Matrix4 m16 = Matrix4.CreateRotationZ(0.8f);
+            Matrix4 m17 = Matrix4.CreateTranslation(0.6f, 0, 0);
+            Matrix4 m18 = m16 * m17;
+            GL.UniformMatrix4(uModelLocation, true, ref m18);
+
+            GL.DrawElements(BeginMode.Triangles, mModel.Indices.Length, DrawElementsType.UnsignedInt, 0);
+
+            Matrix4 m19 = Matrix4.CreateRotationZ(0.8f);
+            Matrix4 m20 = Matrix4.CreateTranslation(-0.6f, 0, 0);
+            Matrix4 m21 = m19 * m20;
+            GL.UniformMatrix4(uModelLocation, true, ref m21);
+
+            GL.DrawElements(BeginMode.Triangles, mModel.Indices.Length, DrawElementsType.UnsignedInt, 0);
+
+            Matrix4 m22 = Matrix4.CreateRotationZ(0.8f);
+            Matrix4 m23 = Matrix4.CreateTranslation(0, 0.6f, 0);
+            Matrix4 m24 = m22 * m23;
+            GL.UniformMatrix4(uModelLocation, true, ref m24);
+
+            GL.DrawElements(BeginMode.Triangles, mModel.Indices.Length, DrawElementsType.UnsignedInt, 0);
+
+            Matrix4 m25 = Matrix4.CreateRotationZ(0.8f);
+            Matrix4 m26 = Matrix4.CreateTranslation(0, -0.6f, 0);
+            Matrix4 m27 = m25 * m26;
+            GL.UniformMatrix4(uModelLocation, true, ref m27);
+
+            GL.DrawElements(BeginMode.Triangles, mModel.Indices.Length, DrawElementsType.UnsignedInt, 0);
+
+            Matrix4 m28 = Matrix4.CreateRotationZ(0.8f);
+            Matrix4 m29 = Matrix4.CreateTranslation(0, 0, 0.5f);
+            Matrix4 m30 = m28 * m29;
+            GL.UniformMatrix4(uModelLocation, true, ref m30);
+
+            GL.DrawElements(BeginMode.Triangles, mModel.Indices.Length, DrawElementsType.UnsignedInt, 0);
+
+            Matrix4 m31 = Matrix4.CreateRotationZ(0.8f);
+            Matrix4 m32 = Matrix4.CreateTranslation(0.3f, 0.3f, 0.5f);
+            Matrix4 m33 = m31 * m32;
+            GL.UniformMatrix4(uModelLocation, true, ref m33);
+
+            GL.DrawElements(BeginMode.Triangles, mModel.Indices.Length, DrawElementsType.UnsignedInt, 0);
+
+            Matrix4 m34 = Matrix4.CreateRotationZ(0.8f);
+            Matrix4 m35 = Matrix4.CreateTranslation(-0.3f, -0.3f, 0.5f);
+            Matrix4 m36 = m34 * m35;
+            GL.UniformMatrix4(uModelLocation, true, ref m36);
+
+            GL.DrawElements(BeginMode.Triangles, mModel.Indices.Length, DrawElementsType.UnsignedInt, 0);
+
+            Matrix4 m37 = Matrix4.CreateRotationZ(0.8f);
+            Matrix4 m38 = Matrix4.CreateTranslation(0.3f, -0.3f, 0.5f);
+            Matrix4 m39 = m37 * m38;
+            GL.UniformMatrix4(uModelLocation, true, ref m39);
+
+            GL.DrawElements(BeginMode.Triangles, mModel.Indices.Length, DrawElementsType.UnsignedInt, 0);
+
+            Matrix4 m40 = Matrix4.CreateRotationZ(0.8f);
+            Matrix4 m41 = Matrix4.CreateTranslation(-0.3f, 0.3f, 0.5f);
+            Matrix4 m42 = m40 * m41;
+            GL.UniformMatrix4(uModelLocation, true, ref m42);
+
+            GL.DrawElements(BeginMode.Triangles, mModel.Indices.Length, DrawElementsType.UnsignedInt, 0);
+
+            Matrix4 m43 = Matrix4.CreateRotationZ(0.8f);
+            Matrix4 m44 = Matrix4.CreateTranslation(0.6f, 0, 0.5f);
+            Matrix4 m45 = m43 * m44;
+            GL.UniformMatrix4(uModelLocation, true, ref m45);
+
+            GL.DrawElements(BeginMode.Triangles, mModel.Indices.Length, DrawElementsType.UnsignedInt, 0);
+
+            Matrix4 m46 = Matrix4.CreateRotationZ(0.8f);
+            Matrix4 m47 = Matrix4.CreateTranslation(-0.6f, 0, 0.5f);
+            Matrix4 m48 = m46 * m47;
+            GL.UniformMatrix4(uModelLocation, true, ref m48);
+
+            GL.DrawElements(BeginMode.Triangles, mModel.Indices.Length, DrawElementsType.UnsignedInt, 0);
+
+            Matrix4 m49 = Matrix4.CreateRotationZ(0.8f);
+            Matrix4 m50 = Matrix4.CreateTranslation(0, 0.6f, 0.5f);
+            Matrix4 m51 = m49 * m50;
+            GL.UniformMatrix4(uModelLocation, true, ref m51);
+
+            GL.DrawElements(BeginMode.Triangles, mModel.Indices.Length, DrawElementsType.UnsignedInt, 0);
+
+            Matrix4 m52 = Matrix4.CreateRotationZ(0.8f);
+            Matrix4 m53 = Matrix4.CreateTranslation(0, -0.6f, 0.5f);
+            Matrix4 m54 = m52 * m53;
+            GL.UniformMatrix4(uModelLocation, true, ref m54);
+
+            GL.DrawElements(BeginMode.Triangles, mModel.Indices.Length, DrawElementsType.UnsignedInt, 0);
+
+            Matrix4 m55 = Matrix4.CreateRotationZ(0.8f);
+            Matrix4 m56 = Matrix4.CreateTranslation(0, 0, -0.5f);
+            Matrix4 m57 = m55 * m56;
+            GL.UniformMatrix4(uModelLocation, true, ref m57);
+
+            GL.DrawElements(BeginMode.Triangles, mModel.Indices.Length, DrawElementsType.UnsignedInt, 0);
+
+            Matrix4 m58 = Matrix4.CreateRotationZ(0.8f);
+            Matrix4 m59 = Matrix4.CreateTranslation(0.3f, 0.3f, -0.5f);
+            Matrix4 m60 = m58 * m59;
+            GL.UniformMatrix4(uModelLocation, true, ref m60);
+
+            GL.DrawElements(BeginMode.Triangles, mModel.Indices.Length, DrawElementsType.UnsignedInt, 0);
+
+            Matrix4 m61 = Matrix4.CreateRotationZ(0.8f);
+            Matrix4 m62 = Matrix4.CreateTranslation(-0.3f, -0.3f, -0.5f);
+            Matrix4 m63 = m61 * m62;
+            GL.UniformMatrix4(uModelLocation, true, ref m63);
+
+            GL.DrawElements(BeginMode.Triangles, mModel.Indices.Length, DrawElementsType.UnsignedInt, 0);
+
+            Matrix4 m64 = Matrix4.CreateRotationZ(0.8f);
+            Matrix4 m65 = Matrix4.CreateTranslation(0.3f, -0.3f, -0.5f);
+            Matrix4 m66 = m64 * m65;
+            GL.UniformMatrix4(uModelLocation, true, ref m66);
+
+            GL.DrawElements(BeginMode.Triangles, mModel.Indices.Length, DrawElementsType.UnsignedInt, 0);
+
+            Matrix4 m67 = Matrix4.CreateRotationZ(0.8f);
+            Matrix4 m68 = Matrix4.CreateTranslation(-0.3f, 0.3f, -0.5f);
+            Matrix4 m69 = m67 * m68;
+            GL.UniformMatrix4(uModelLocation, true, ref m69);
+
+            GL.DrawElements(BeginMode.Triangles, mModel.Indices.Length, DrawElementsType.UnsignedInt, 0);
+
+            Matrix4 m70 = Matrix4.CreateRotationZ(0.8f);
+            Matrix4 m71 = Matrix4.CreateTranslation(0.6f, 0, -0.5f);
+            Matrix4 m72 = m70 * m71;
+            GL.UniformMatrix4(uModelLocation, true, ref m72);
+
+            GL.DrawElements(BeginMode.Triangles, mModel.Indices.Length, DrawElementsType.UnsignedInt, 0);
+
+            Matrix4 m73 = Matrix4.CreateRotationZ(0.8f);
+            Matrix4 m74 = Matrix4.CreateTranslation(-0.6f, 0, -0.5f);
+            Matrix4 m75 = m73 * m74;
+            GL.UniformMatrix4(uModelLocation, true, ref m75);
+
+            GL.DrawElements(BeginMode.Triangles, mModel.Indices.Length, DrawElementsType.UnsignedInt, 0);
+
+            Matrix4 m76 = Matrix4.CreateRotationZ(0.8f);
+            Matrix4 m77 = Matrix4.CreateTranslation(0, 0.6f, -0.5f);
+            Matrix4 m78 = m76 * m77;
+            GL.UniformMatrix4(uModelLocation, true, ref m78);
+
+            GL.DrawElements(BeginMode.Triangles, mModel.Indices.Length, DrawElementsType.UnsignedInt, 0);
+
+            Matrix4 m79 = Matrix4.CreateRotationZ(0.8f);
+            Matrix4 m80 = Matrix4.CreateTranslation(0, -0.6f, -0.5f);
+            Matrix4 m81 = m79 * m80;
+            GL.UniformMatrix4(uModelLocation, true, ref m81);
 
             GL.DrawElements(BeginMode.Triangles, mModel.Indices.Length, DrawElementsType.UnsignedInt, 0);
 
